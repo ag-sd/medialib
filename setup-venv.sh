23fcc689e300c337b11c1cddfe2bb947d885f773
Sheldon DSouza <sheldon.s.d@gmail.com>
2024-01-07 17:06:48 -0500

    Query Parsing Module

---
+++ setup-venv.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+VENV_DIR="venv"
+
+# Clean up
+rm -rf $VENV_DIR
+
+# Create venv dir
+mkdir -v -p $VENV_DIR
+
+# Install Python venv
+echo "Creating a Python virtual environment"
+python3 -m venv $VENV_DIR
+source "$VENV_DIR/bin/activate"
+echo "Upgrading virtual environment"
+python3 -m pip install --upgrade pip
+
+# Install dependencies
+echo "Install dependency : PYQT6"
+pip install pyqt6
+# Install dependencies
+echo "Install dependency : pyparsing"
+pip install pyparsing
+#pip install psutil
